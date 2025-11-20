71b871c6103a6c71ca5731fc54e0272652997f74
“Iblal” <iblalahmed12@gmail.com>
2025-11-20 16:39:33 +0000

    removed duplicate project

--- MegaMart.Presentation/MegaMart.Persistence/AssemblyReference.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-namespace MegaMart.Persistence;
-
-public static class AssemblyReference
-{
-    public static readonly Assembly Assembly = typeof(AssemblyReference).Assembly;
-}
